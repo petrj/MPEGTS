456127e6b184676582508723517a0a161d8a0011
petr.janousek <petr.janousek@zat.cz>
2026-08-27 09:35:04 +0200

    nuget ver 1.0.27 + publish script

---
+++ PublishNugetPackage.ps1
@@ -0,0 +1,34 @@
+﻿Set-Location $PSScriptRoot
+
+function Publish-Nuget
+{
+    [CmdletBinding()]
+    param (
+        [string]$ProjectName,
+        [string]$Token,
+        [string]$SolutionPath
+    )
+    process
+    {
+        $ProjectPath = Join-Path $SolutionPath -ChildPath "$ProjectName\$ProjectName.csproj"
+
+        dotnet build $ProjectPath -c Release #/p:PackageVersion=$packageVersion
+
+        $fName = Join-Path $SolutionPath -ChildPath "$ProjectName\bin\Release\$ProjectName.$PackageVersion.nupkg"
+
+        dotnet nuget push $fName -k $Token --source "github"  --timeout 3000 # --skip-duplicate
+    }
+}
+
+[xml]$projXML = Get-content -Path ./MPEGTS/MPEGTS.csproj
+
+$packageVersion = $projXML.Project.PropertyGroup.PackageVersion
+
+Write-Host "Publishing MPEGTS package version $packageVersion"
+
+
+Write-Host "Enter Android acces token: " -NoNewline
+
+$token = Read-host
+
+Publish-Nuget -ProjectName "MPEGTS" -SolutionPath $PSScriptRoot -Token $token
